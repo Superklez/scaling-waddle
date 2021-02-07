84a9cfb24f44343d8afadc2cec864efa764eb008
Superklez <bjlherrera@gmail.com>
2021-02-07 17:02:21 +0800

    Reverted back to previous numpy index notation
    I realized the notation I was using was for indexing images using numpy, where the notation is F[y, x], instead of what others may use in other python tasks, where F[x, y] is the notation.

--- experiments/heat_flow.py
+++ experiments/heat_flow.py
@@ -10,10 +10,10 @@ kappa = 237 # conductivity
 C = 900     # specific heat
 rho = 2700  # density
 
-T = np.zeros((2, Nx))
-Tpl = np.zeros((31, Nx))
+T = np.zeros((Nx, 2))
+Tpl = np.zeros((Nx, 31))
 
-T[0, 1:-1] = 100
+T[1:-1, 0] = 100
 #T[0, 0] = 0; T[0, 1] = 0    # redundant
 #T[-1, 0] = 0; T[-1, 1] = 0  # redundant
 
@@ -21,17 +21,17 @@ const = kappa / (C * rho) * Dt / (Dx * Dx)
 
 m = 1
 for t in range(1, Nt):
-    T[1, 1:-1] = T[0, 1:-1] + const * (T[0, 2:] + T[0, :-2] - 2 * T[0, 1:-1])
+    T[1:-1, 1] = T[1:-1, 0] + const * (T[2:, 0] + T[:-2, 0] - 2 * T[1:-1, 0])
     if t == 1 or t % 500 == 0:
-        Tpl[m, 1:-1:2] = T[1, 1:-1:2]
+        Tpl[1:-1:2, m] = T[1:-1:2, 1]
         m += 1
-    T[0, 1:-1] = T[1, 1:-1]
+    T[1:-1, 0] = T[1:-1, 1]
 
-x = list(range(1, Nx-1, 2))
 y = list(range(1, 30))
+x = list(range(1, Nx-1, 2))
 X, Y = np.meshgrid(x, y)
 
-Z = Tpl[Y, X]
+Z = Tpl[X, Y]
 
 fig = plt.figure(figsize=(10, 10), dpi=100)
 ax = plt.axes(projection ='3d')
