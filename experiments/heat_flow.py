d7a53944a01aadf5a299a5093c43924a2ea3864c
Superklez <bjlherrera@gmail.com>
2021-02-07 14:00:54 +0800

    Fixed notations for numpy indexing

--- experiments/heat_flow.py
+++ experiments/heat_flow.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 Nx = 101
 Nt = 15000
 Dx = 0.03
-Dt = 0.9
+Dt = 1.5
 
 kappa = 237 # conductivity
 c = 900     # specific heat
@@ -14,31 +14,30 @@ T = np.zeros((Nx, 2))
 Tpl = np.zeros((Nx, 31))
 
 T[1:-1, 0] = 100
-T[0, 0] = 0; T[0, 1] = 0
-T[Nx-1, 0] = 0; T[Nx-1, 1] = 0
+#T[0, 0] = 0; T[0, 1] = 0    # redundant
+#T[-1, 0] = 0; T[-1, 1] = 0  # redundant
 
 const = kappa / (c * rho) * Dt / (Dx * Dx)
 
 m = 1
 for t in range(1, Nt):
-    T[1:-1, 1] = T[1:-1, 0] + const * (T[2:, 0] + T[:-2, 0] - 2*T[1:-1, 0])
+    T[1:-1, 1] = T[1:-1, 0] + const * (T[2:, 0] + T[:-2, 0] - 2 * T[1:-1, 0])
     if t == 1 or t % 500 == 0:
         Tpl[1:-1:2, m] = T[1:-1:2, 1]
         m += 1
     T[1:-1, 0] = T[1:-1, 1]
 
-x = list(range(1, Nx-1, 2))
-y = list(range(1, 30))
+x = list(range(1, 30))
+y = list(range(1, Nx-1, 2))
 X, Y = np.meshgrid(x, y)
 
-Z = Tpl[X, Y]
-
+Z = Tpl[Y, X]
 
 fig = plt.figure(figsize=(10, 10), dpi=100)
-ax = fig.add_subplot(111, projection='3d')
+ax = plt.axes(projection ='3d')
 ax.plot_wireframe(X, Y, Z, color='royalblue')
-ax.view_init(30, 45)
-ax.set_xlabel('Position')
-ax.set_ylabel('Time')
+ax.view_init(30, -45)
+ax.set_xlabel('Time')
+ax.set_ylabel('Position')
 ax.set_zlabel('Temperature')
 plt.show()
