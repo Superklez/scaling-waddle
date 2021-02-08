b3886893e5be6b70a1eb1ba044fa42c6b9628afd
Superklez <bjlherrera@gmail.com>
2021-02-08 20:44:33 +0800

    Update heat_flow.py

--- experiments/heat_flow.py
+++ experiments/heat_flow.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 Nx = 101
 Nt = 15000
-Dx = 0.03
-Dt = 1.5
+Dx = 0.03   # diverges at 0.015
+Dt = 1.5    # diverges at 4.65
 
 kappa = 237 # conductivity
-C = 900     # specific heat
+c = 900     # specific heat
 rho = 2700  # density
 
 T = np.zeros((Nx, 2))
@@ -17,11 +17,12 @@ T[1:-1, 0] = 100
 #T[0, 0] = 0; T[0, 1] = 0    # redundant
 #T[-1, 0] = 0; T[-1, 1] = 0  # redundant
 
-const = kappa / (C * rho) * Dt / (Dx * Dx)
+eta = kappa / (c * rho) * Dt / (Dx * Dx)
+#print(eta)  # diverges if eta > 0.5
 
 m = 1
 for t in range(1, Nt):
-    T[1:-1, 1] = T[1:-1, 0] + const * (T[2:, 0] + T[:-2, 0] - 2 * T[1:-1, 0])
+    T[1:-1, 1] = T[1:-1, 0] + eta * (T[2:, 0] + T[:-2, 0] - 2 * T[1:-1, 0])
     if t == 1 or t % 500 == 0:
         Tpl[1:-1:2, m] = T[1:-1:2, 1]
         m += 1
